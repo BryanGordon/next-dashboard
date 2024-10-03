6fca1d9fa08f70f3185bf2801518a6d9c7a34c51
Bryan Gordon <bryanbge92@gmail.com>
2024-10-03 14:37:41 -0500

    Update landing page
    - Add font
    - Add Image component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import styles from './ui/home.module.css'
+import { muslish } from './ui/fonts'
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from './ui/home.module.css'
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -12,7 +14,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={` ${muslish.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -27,7 +29,21 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image 
+            src='/hero-desktop.png' 
+            alt='Screenshots of the dashboard' 
+            width={1000}
+            height={620}
+            className='hidden md:block'
+          />
+
+          <Image
+            src='/hero-mobile.png'
+            alt='Screenshots of dashboard'
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
