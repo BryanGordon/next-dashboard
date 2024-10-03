5f1117a461f0b5aedc60420f266e49c403d23eed
Bryan Gordon <bryanbge92@gmail.com>
2024-10-03 14:37:58 -0500

    Import fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Outfit, Mulish } from 'next/font/google'
+
+export const outfit = Outfit({ subsets: ['latin'] })
+
+export const muslish = Mulish ({ subsets: ['latin'] ,weight: ['400', '700'] })
